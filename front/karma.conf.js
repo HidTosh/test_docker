85846cb5818abdab913fbcd9ab053160f1f1f883
mhandrahani <mhandrahani91@gmail.com>
2023-10-23 15:58:40 +0200

    update

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -28,7 +28,6 @@ module.exports = function (config) {
       dir: require('path').join(__dirname, './coverage/bobapp'),
       subdir: '.',
       reporters: [
-        { type: 'html' },
         { type: 'lcov' },
         { type: 'text-summary' }
       ]
